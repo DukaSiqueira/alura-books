1eb30d06348c10efa86a3222cca4f6195afaa513
Eduardo Siqueira <53954835+DukaSiqueira@users.noreply.github.com>
2024-05-04 11:37:52 -0300

    Card recommendation

--- src/components/LatestReleases/index.js
+++ src/components/LatestReleases/index.js
@@ -1,7 +1,8 @@
 import { books } from './dataLatestReleases';
 import styled from 'styled-components';
 import { Title } from '../Title';
-
+import CardRecommendation from '../CardRecommendation';
+import Livro from '../../images/livro.png';
 
 const LatestReleasesContainer = styled.section`
     background-color: #EBECEE;
@@ -21,12 +22,22 @@ const NewBooksContainer = styled.div`
 function LatestReleases() {
     return (
         <LatestReleasesContainer>
-            <Title color="#000">Últimos Lançamentos</Title>
+            <Title 
+                color="#EB9B00" 
+                textSize="36px"
+            >Últimos Lançamentos</Title>
             <NewBooksContainer>
                 { books.map( book => (
                     <img src={book.src} alt='Book'/>
                 ))}
             </NewBooksContainer>
+            <CardRecommendation 
+                title="Talvez você se enteresse por..."
+                subTitle="Mestre dos Magos"
+                description="Sem descrição né"
+                img={Livro}
+                alt="Logo"
+            />
         </LatestReleasesContainer>
     );
 }
